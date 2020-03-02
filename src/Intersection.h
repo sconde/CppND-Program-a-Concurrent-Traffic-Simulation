07d83b4f653e190942df66f5dbbfcd1ad2a92eca
Sidafa Conde <sconde@sandia.gov>
2020-03-01 19:38:57 -0500

    Task FP.6

--- src/Intersection.h
+++ src/Intersection.h
@@ -6,6 +6,7 @@
 #include <mutex>
 #include <memory>
 #include "TrafficObject.h"
+#include <TrafficLight.h>
 
 // forward declarations to avoid include cycle
 class Street;
@@ -14,7 +15,7 @@ class Vehicle;
 // auxiliary class to queue and dequeue waiting vehicles in a thread-safe manner
 class WaitingVehicles
 {
-public:
+  public:
     // getters / setters
     int getSize();
 
@@ -22,7 +23,7 @@ public:
     void pushBack(std::shared_ptr<Vehicle> vehicle, std::promise<void> &&promise);
     void permitEntryToFirstInQueue();
 
-private:
+  private:
     std::vector<std::shared_ptr<Vehicle>> _vehicles;          // list of all vehicles waiting to enter this intersection
     std::vector<std::promise<void>> _promises; // list of associated promises
     std::mutex _mutex;
@@ -30,7 +31,7 @@ private:
 
 class Intersection : public TrafficObject
 {
-public:
+  public:
     // constructor / desctructor
     Intersection();
 
@@ -45,7 +46,7 @@ public:
     void vehicleHasLeft(std::shared_ptr<Vehicle> vehicle);
     bool trafficLightIsGreen();
 
-private:
+  private:
 
     // typical behaviour methods
     void processVehicleQueue();
@@ -54,6 +55,7 @@ private:
     std::vector<std::shared_ptr<Street>> _streets;   // list of all streets connected to this intersection
     WaitingVehicles _waitingVehicles; // list of all vehicles and their associated promises waiting to enter the intersection
     bool _isBlocked;                  // flag indicating wether the intersection is blocked by a vehicle
+    TrafficLight _trafficLight;
 };
 
 #endif
